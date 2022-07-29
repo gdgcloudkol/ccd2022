d5c1ba624ddf435c470b94db6342bdd05295bc6a
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2022-07-29 23:55:16 +0530

    feat/cards

--- src/Components/Speakers/Speakers.tsx
+++ src/Components/Speakers/Speakers.tsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from 'react'
 import Socials from '../Socials/Socials'
 import Title from '../Title/Title'
-import speakersData from './speakersData.json'
+// import speakersData from './speakersData.json'
 
 const Speakers = () => {
+  const [speakersDetails, setSpeakersDetails] = useState<any[]>([])
+
+  useEffect(() => {
+    fetch('https://sessionize.com/api/v2/kirmfltc/view/Speakers')
+      .then((response) => response.json())
+      .then((data) => {
+        setSpeakersDetails(data)
+        console.log(data)
+      })
+      .catch((error) => console.log(error))
+  }, [])
+
   const imageBorderStyle = {
     // border: '1px solid #eaeaea',
     backgroundImage: `url("https://www.linkpicture.com/q/favicon-1.svg")`,
@@ -15,36 +28,35 @@ const Speakers = () => {
     <>
       <Title heading="Our Amazing Speakers" />
       <div
-        className="grid sm:grid-cols-1 md:grid-cols-4 grid-flow-row place-items-center "
+        className="grid 
+        sm:grid-cols-1 md:grid-cols-3 grid-flow-row place-items-center p-5 lg:grid-cols-4"
         id="speakers-grid"
       >
-        {speakersData.map((speaker, id) => (
-          <div
-            key={id}
-            className="flex sm:w-100 flex-col rounded-xl items-center mb-4 shadow-lg md:w-5/6"
-          >
-            <div style={imageBorderStyle}>
-              <img
-                src={speaker.image}
-                alt="profile"
-                className="rounded-full m-2.5 w-40 h-40 "
-              />
+        {speakersDetails &&
+          speakersDetails.map((speaker) => (
+            <div
+              key={speaker.id}
+              className="flex sm:w-100 flex-col rounded-xl items-center mb-4 shadow-lg md:w-5/6 p-3 h-96 relative"
+            >
+              <div style={imageBorderStyle}>
+                <img
+                  src={speaker.profilePicture}
+                  alt="profile"
+                  className="rounded-full m-2.5 w-40 h-40 "
+                />
+              </div>
+              <h3 className="text-xl font-bold">{speaker.fullName}</h3>
+              <h3 className="text-sm text-center text-lightGrey font-semibold ">
+                {speaker.tagLine}
+              </h3>
+              {/* <div className=" overflow-y-hidden">
+                <p className="text-sm text-lightGrey text-center py-3 ">
+                  {speaker.bio}
+                </p>
+          </div> */}
+              <Socials links={[...speaker?.links]} className="absolute bottom-4" />
             </div>
-            <h3 className="text-xl font-bold">{speaker.name}</h3>
-            <h3 className="text-xl text-lightGrey font-semibold ">
-              {speaker.position}
-            </h3>
-            <p className="text-sm text-lightGrey text-center py-3">
-              {speaker.description}
-            </p>
-            <Socials
-              twitterLink={speaker.twitterLink}
-              githubLink={speaker.githubLink}
-              linkedinLink={speaker.linkedinLink}
-              facebookLink={speaker.facebookLink}
-            />
-          </div>
-        ))}
+          ))}
       </div>
     </>
   )
